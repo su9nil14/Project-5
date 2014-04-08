f3221510c6e84055ac9ac3b246325c9d3e7a4c73
Sunil Gautam <gautams@tcd.ie>
2014-04-08 15:48:07 +0100

    fixed no path test
    also changes in dfs algorithm

--- Mazesolver/src/DFSMazeRunner.java
+++ Mazesolver/src/DFSMazeRunner.java
@@ -1,6 +1,8 @@
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Stack;
+
 
 /**
  * This implements a DFSMaze Runner Algorithm modified from the class
@@ -10,15 +12,15 @@ import java.util.Stack;
  * @Version 01/04/2014 18:40
  */
 public class DFSMazeRunner<MC extends MazeCell> extends MazeRunner<MC> {
-	
+
 	private Stack<MC> stack; //stackited nodes for DFSalgorithm
-	private Stack<MC> sol; //Recorded nodes for solution path
-			//used the deque data structure for easier printing of solution path
-	
+	private LinkedList<MC> sol; //Recorded nodes for solution path
+	//used the deque data structure for easier printing of solution path
+
 	public DFSMazeRunner() //Constructor to initilise: stack & sol
 	{
-		stack = new Stack<MC>();
-		sol = new Stack<MC>();
+		stack=new Stack<MC>();
+		sol = new LinkedList<MC>();
 	}
 
 	/**
@@ -32,74 +34,60 @@ public class DFSMazeRunner<MC extends MazeCell> extends MazeRunner<MC> {
 	 * @param Maze<MC> maze		The maze to solve.
 	 * @param PrintWriter writer	The print writer on which to output the maze solution. (For Testing also)
 	 */
+
 	public void solveMaze(Maze<MC> maze, PrintWriter writer) 
 	{
-		
+
 		int cellsExpanded = 0;
 		MC curCell = maze.getStart();
-		stack.push(curCell);		
-		//curCell.setState(MazeCell.CellState.visited);		
-		//System.out.println(curCell);
-
-		while (!stack.isEmpty()) {
-			cellsExpanded++;
-			
-			curCell = stack.peek(); //Set the current cell to top of stack
-			//System.out.println(curCell);
-			sol.push(curCell); //Add the cell to be removed to sol stack to print solution path.
-			
-			if(stack.peek()!=null) //If cells on the top of the stack is not empty
-				stack.pop(); //Remove cell from stack 
-			else //If there is no solution path write and return
-			{
-				writer.print("No solution path found");
-				return;
-			}
-			
-			if (maze.getDonut() == curCell) //If the current cell is the donut 
-			{
-				//System.out.println("maze solved"+maze.getDonut());
-				break;
-
-			} 
-			else //The current cell is not the solution, continue search
-			{ 
-				Iterator<MC> i = maze.getNeighbors(curCell);
-				MC next = null;
-				
-				while (i.hasNext()) 
-				{
-					next = i.next();
-
-					if (next.getState() == MazeCell.CellState.notVisited) 
-					{
-						
-						//Neighbour found, set state to visit in progress and add to stack
-						next.setState(MazeCell.CellState.visited);
-						stack.push(next);
-						//System.out.println(next);
-											
-					}
-					
-				}
-				// Set current cell's state to visited, prepared to be popped.
-				curCell.setState(MazeCell.CellState.visited);
-				//stack.pop();
-			}
-			
-		}
+		stack.push(curCell);
+		curCell.setState(MazeCell.CellState.visited);
 		
+		 while(!stack.empty())
+	        {
+			 cellsExpanded++;
+			    curCell = stack.pop();
+			    sol.add(curCell);
+	            //System.out.println(curCell + " has been visited.");
+
+	            // Get this vertex's list of previousConnections (starting at the end of the list).
+	            Iterator<MC> list = maze.getNeighbors(curCell);
+	           
+	            // Loop through the list backwards.  This is done so destinations will come out in numerical order.
+	            while(list.hasNext())
+	            {
+	            	MC listItr = list.next();
+	                MC destination = listItr;
+
+	                // If the node has not already been visited, add it to the stack for processing.
+	                if(destination.getState()!=MazeCell.CellState.visited)
+	                {
+	                	destination.setState(MazeCell.CellState.visited);
+	                    stack.push(destination);
+	                }
+	                if(curCell==maze.getDonut()){
+	                	//System.out.println("found=== "+curCell + "==" + maze.getDonut());
+	                	print(curCell,maze,writer,cellsExpanded);//////
+	                	break;
+	                }
+	            }
+
+	        }
+		
+	}
+
+	private void print(MC curCell, Maze<MC> maze, PrintWriter writer,int cellsExpanded) {
 		//Output string for solution path
 		int pathLength = 0; //Path length for output
 		String solutionString = "";
-		writer.print("DFSSolutionPath: "); //Puts this initial string into writer for output
 		
-		while (maze.getStart()!=sol.lastElement()) //Iterates through the solution deque, end is start of maze
+		writer.print("DFS SolutionPath: "); //Puts this initial string into writer for output
+
+		while (sol.getLast() != maze.getStart()) //Iterates through the solution deque, end is start of maze
 		{
-			MC currentCell = sol.lastElement();
-			//MC last=sol.lastElement();
-			sol.remove(currentCell);
-			MC possibleNeighbour = sol.lastElement();
+			MC currentCell = sol.getLast();
+			sol.removeLast();
+			MC possibleNeighbour = sol.getLast();///////////
 
 			if (isAdjacentNeighbour(currentCell, possibleNeighbour, maze)) 
 			{
@@ -111,24 +99,21 @@ public class DFSMazeRunner<MC extends MazeCell> extends MazeRunner<MC> {
 				
 				pathLength++;
 			} 
-			else 
+			else //o/w not a neighbour, remove previous from deque, re-add to last in queue
 			{
-				sol.remove(sol.lastElement());
-				//sol.pop();
-				Stack<MC> last=new Stack<MC>();
-				last.push(currentCell);
-				MC lastElement=last.lastElement();
-				sol.push(lastElement);
+				sol.removeLast();				
+				sol.addLast(currentCell);
+				
+
 			}
 		}
 		
 		solutionString = maze.getStart() + " " + solutionString; //Add start of maze to writer
-
 		writer.print(solutionString + "\n"); //The solution path string printed for tests
 		writer.print(pathLength + "\n"); //Length of the path printed for tests
 		writer.print(cellsExpanded); //Cells expanded printed for tests
+		
 	}
-
 
 	/**
 	 * Method for isAdjacentNeighbour
@@ -143,13 +128,28 @@ public class DFSMazeRunner<MC extends MazeCell> extends MazeRunner<MC> {
 		while (iterator.hasNext())
 		{
 			MC neighbour = iterator.next();
-			
+
 			//If the adjacent cell is a neighbour of CurrentCell, return true
 			if (adjacentCell == neighbour) return true;
-			
+
 		}
-		
+
 		return false; //Return false if all adjacent cells checked and no neighbours found
+	}
+	private class SolutionPathInfo {
+		/**
+		 * Link to the next cell in the solution path.
+		 */
+		public MC nextInSolution;
+	}
+	
+	private SolutionPathInfo getSolutionPathInfo(MazeCell curCell)
+	{
+		if(null == curCell.getExtraInfo()) {
+			curCell.setExtraInfo( new SolutionPathInfo() );
+		}
+
+		return (SolutionPathInfo)curCell.getExtraInfo();
 	}
 
 }
